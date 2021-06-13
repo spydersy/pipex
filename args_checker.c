a9e405ab5722165ab9af32b750e5067ad78a89b1
spydersy <acbe1325@aol.com>
2021-06-13 12:54:17 -0400

    2017

---
+++ args_checker.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   args_checker.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelarif <abelarif@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/13 10:57:35 by abelarif          #+#    #+#             */
+/*   Updated: 2021/06/13 10:58:32 by abelarif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void    files_checker(void)
+{
+    g_args.infile = open(g_args.argv[1], O_RDONLY);
+    g_args.outfile = open(g_args.argv[4], O_WRONLY | O_CREAT, 0666);
+    if (g_args.infile < 0)
+    {
+        ft_error( "No such file (infile)", 1);
+    }
+    if (g_args.outfile < 0)
+    {
+        ft_error( "cant open output file", 1);
+    }
+}
+
+void    cmd_checker(void)
+{
+    
+}
+
+void    args_checker(void)
+{
+    files_checker();
+    cmd_checker();
+}
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: abelarif <abelarif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 07:51:51 by abelarif          #+#    #+#             */
-/*   Updated: 2021/06/13 09:57:20 by abelarif         ###   ########.fr       */
+/*   Updated: 2021/06/13 11:05:41 by abelarif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,29 +49,4 @@ void    free_args(void)
     free(g_args.envp);
     g_args.envp = NULL;
 
-}
-
-void    files_checker(void)
-{
-    g_args.infile = open(g_args.argv[1], O_RDONLY);
-    g_args.outfile = open(g_args.argv[4], O_WRONLY | O_CREAT, 0666);
-    if (g_args.infile < 0)
-    {
-        ft_error( "No such file (infile)", 1);
-    }
-    if (g_args.outfile < 0)
-    {
-        ft_error( "cant open output file", 1);
-    }
-}
-
-void    cmd_checker(void)
-{
-    
-}
-
-void    args_checker(void)
-{
-    files_checker();
-    cmd_checker();
 }

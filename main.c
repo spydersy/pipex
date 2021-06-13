be741bc3e7b0194b11a1c91863247063caf579dc
spydersy <acbe1325@aol.com>
2021-06-13 10:55:34 -0400

    segfault : fixed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abelarif <abelarif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 06:56:11 by abelarif          #+#    #+#             */
-/*   Updated: 2021/06/13 10:43:46 by abelarif         ###   ########.fr       */
+/*   Updated: 2021/06/13 10:53:01 by abelarif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	main(int argc, char *argv[], char *envp[])
 {
+	dup_args(argv, envp);
 	if (argc == 5)
 	{
-		dup_args(argv, envp);
 		pipex();
 		free_args();
 		return (0);

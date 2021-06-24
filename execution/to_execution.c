b29a3b771b36d25a18997c16f67d8691b1270b99
spydersy <acbe1325@aol.com>
2021-06-24 13:05:06 +0100

    TODO : COMPLET EXECUTION

--- execution/to_execution.c
+++ execution/to_execution.c
@@ -6,13 +6,27 @@
 /*   By: abelarif <abelarif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/21 11:49:42 by abelarif          #+#    #+#             */
-/*   Updated: 2021/06/24 12:57:34 by abelarif         ###   ########.fr       */
+/*   Updated: 2021/06/24 13:04:07 by abelarif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
+void	first_command(t_data data)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		ft_error(NULL, 1);
+	if (pid == 0)
+	{
+		
+	}
+}
+
 t_data	to_execution(t_data data)
 {
+	first_command(data);
 	return (data);
 }

ca3a2d553b42e8ca5358906aaa54c5a81be5fae9
spydersy <acbe1325@aol.com>
2021-06-24 18:46:48 +0100

    DONE : lab.c

--- lab.c
+++ lab.c
@@ -6,7 +6,7 @@
 /*   By: abelarif <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 14:17:53 by abelarif          #+#    #+#             */
-/*   Updated: 2021/06/24 18:15:23 by abelarif         ###   ########.fr       */
+/*   Updated: 2021/06/24 18:45:46 by abelarif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,37 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+int		main(void)
+{
+	int 	pipes[2];
+	char	*argv[] = {"ls", "-l", "-a", NULL};
+	pid_t	pid;
+
+	if (pipe(pipes) == -1)
+		return (-1);
+	pid = fork();
+	if (pid == 0)
+	{
+		dup2(pipes[1], 1);
+	//	close(pipes[0]);
+		close(pipes[1]);
+		if (execve("/bin/ls", argv, NULL) == -1)
+			printf("ERROR EXECVE : ls\n");
+	}
+	pid = fork();
+	if (pid == 0)
+	{
+		char *gg[] = {"grep", "main", NULL};
+		dup2(pipes[0], 0);
+		close(pipes[0]);
+	//	close(pipes[1]);
+		if (execve("/usr/bin/grep", gg, NULL) == -1)
+			printf("ERROR EXECVE : grep");
+	}
+	return (0);
+}
+
+/*
 int		main()
 {
 	int		fd;
@@ -37,4 +68,4 @@ int		main()
 	read(0, buffer, 10);
 	printf("[%s]\n", buffer);
 	return (0);
-}
+}*/
